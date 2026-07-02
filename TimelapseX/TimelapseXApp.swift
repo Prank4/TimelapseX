ced454ac3d549e07ef45c14b83caa0c909abd446
Prankur <44020848+Prank4@users.noreply.github.com>
2026-07-02 13:52:32 +0530

    Initial Commit

---
+++ TimelapseX/TimelapseXApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimelapseXApp.swift
+//  TimelapseX
+//
+//  Created by Prank on 02/07/26.
+//
+
+import SwiftUI
+
+@main
+struct TimelapseXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
